7fdec02430a3f033c08461ffe1cfa57184490735
vDrDv <vander.dev@outlook.com>
2023-11-05 01:56:38 -0300

    Entrega do projeto

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+import random
+
+heroi = input("Digite onome do seu HEROI: ")
+xp = random.randint(1000, 18001) 
+
+if xp <= 1000:
+    nivel = "Ferro"
+elif xp >= 1001 and xp < 2001:
+    nivel = "Bronze"
+elif xp >= 2001 and xp < 5001:
+    nivel = "Prata"
+elif xp >= 5001 and xp < 6001:
+    nivel = "Quase OURO"
+elif xp >= 6001 and xp < 7001:
+    nivel = "Ouro"
+elif xp >= 7001 and xp < 8001:
+    nivel = "Platina"
+elif xp >= 8001 and xp < 9001:
+    nivel = "Ascendente"
+elif xp >= 9001 and xp < 10001:
+    nivel = "Imortal"
+elif xp >= 10001:
+    nivel = "Radiante"
+
+print("O Herói de nome " + heroi + " esta no nível de " + nivel + ". Com uma experiência total de: " + str(xp) + ".")
+
+
